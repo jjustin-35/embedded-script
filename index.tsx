ec92062409703305878b8a9b4b2e824d15e0e108
jjustin-chen <justin.chen@kdanmobile.com>
2023-12-08 19:00:08 +0800

    update: init babel

--- index.tsx
+++ index.tsx
@@ -1,5 +1,5 @@
 import { createRoot } from "react-dom/client";
-import Footer from "./components/Footer";
+import Footer from "./src/components/Footer";
 
 const footerNode = document.createElement("div");
 const footerRoot = createRoot(footerNode);
