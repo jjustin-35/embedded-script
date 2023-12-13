a8fc512b66b24dca119a8115c35ea38e8be33f95
jjustin-chen <justin.chen@kdanmobile.com>
2023-12-13 09:22:27 +0800

    update: remove old code

--- src/components/header/dropdown/index.ts
+++
@@ -1,8 +0,0 @@
-class Dropdown extends HTMLDivElement {
-  constructor() {
-    super();
-    this.innerHTML = `<h1>Dropdown</h1>`;
-  }
-}
-
-customElements.define('header-dropdown', Dropdown, { extends: 'div' });
