d69cf5f5aa615cdf80907d2344b857ab08482d79
jjustin-chen <justin.chen@kdanmobile.com>
2023-12-14 14:03:34 +0800

    update: remove header

--- src/index.tsx
+++ src/index.tsx
@@ -2,24 +2,7 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import "./config/i18n";
 import GlobalStyle from "./constants/globalStyle";
-import Header from "./containers/Header";
 import Footer from "./components/Footer";
-
-const headerNode = document.createElement("div");
-const headerRoot = createRoot(headerNode);
-headerRoot.render(
-  <>
-    <GlobalStyle />
-    <Header />
-  </>
-);
-
-const firstChild = document.body.firstChild;
-if (firstChild) {
-  document.body.insertBefore(headerNode, firstChild);
-} else {
-  document.body.appendChild(headerNode);
-}
 
 const footerNode = document.createElement("div");
 const footerRoot = createRoot(footerNode);
