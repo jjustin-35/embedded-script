ff5fa1a9e64997e8820e71bc938302b53b657979
jjustin-chen <justin.chen@kdanmobile.com>
2023-12-14 15:07:01 +0800

    update: font family

---
+++ src/constants/fontFamily.tsx
@@ -0,0 +1,10 @@
+export default {
+  ClearSans:
+    "https://files.kdanmobile.com/web-assets/common/fonts/clearsans/css/clear-sans.css",
+  NotoSansJP:
+    "https://fonts.googleapis.com/css2?family=Noto+Sans&family=Noto+Sans+JP:wght@300;400;500&display=swap",
+  NotoSansTC:
+    "https://fonts.googleapis.com/css2?family=Noto+Sans&family=Noto+Sans+TC:wght@300;400;700&display=swap",
+  NotoSansSC:
+    "https://fonts.googleapis.com/css2?family=Noto+Sans&family=Noto+Sans+SC:wght@300;400;700&display=swap",
+};
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,25 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import "./config/i18n";
+import fontFamily from "./constants/fontFamily";
 import GlobalStyle from "./constants/globalStyle";
 import Footer from "./components/Footer";
+
+const { ClearSans, NotoSansTC, NotoSansSC, NotoSansJP } = fontFamily;
+
+const FontLinks = () => (
+  <>
+    <link href={ClearSans} rel="stylesheet" />
+    <link href={NotoSansTC} rel="stylesheet" />
+    <link href={NotoSansSC} rel="stylesheet" />
+    <link href={NotoSansJP} rel="stylesheet" />
+  </>
+);
+
+const headNode = document.createElement("div");
+const headRoot = createRoot(headNode);
+headRoot.render(<FontLinks />);
+document.head.appendChild(headNode);
 
 const footerNode = document.createElement("div");
 const footerRoot = createRoot(footerNode);
